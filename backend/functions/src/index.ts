6bb6677c49ddaaa04668285998fada8ff56ed20b
Woosang Kang <wk228@cornell.edu>
2021-03-14 13:07:32 +0900

    added survey batch write feature

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,7 +1,10 @@
 import addResponse from "./users/add-response";
 import createCourse from "./courses/create-course";
 import createSurveyCsv from "./courses/data-to-csv";
+import seedSurveyData from "./users/add-response-batch";
 
 export const newCourse = createCourse;
 export const newSurvey = addResponse;
 export const newCsv = createSurveyCsv;
+
+export const seedData = seedSurveyData;
---
+++ backend/functions/src/utils.ts
@@ -0,0 +1,9 @@
+import { FirestoreCourseDoc } from "./firestore-types";
+
+export const idInCourseList = (
+	id: string,
+	docSnapshot: FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>
+): boolean => {
+	const { studentList } = docSnapshot.data() as FirestoreCourseDoc;
+	return studentList.includes(id);
+};
