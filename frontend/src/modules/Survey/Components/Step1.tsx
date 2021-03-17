225a8e68f766c7bc63d38eac6573eba8ca111e31
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-03-16 21:24:54 -0400

    changed styledcontainer to 2 versions, 1 with big bubble background, 1 with smaller bubbles according to figma

---
+++ frontend/src/modules/Survey/Components/Step1.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { StepProps } from 'Survey/Types'
+import {
+  StyledContainer,
+  StyledLeftPanel,
+  StyledRightPanel,
+  StyledLogo,
+  StyledWhiteActionText,
+  StyledTeamPic,
+  StyledTitleWrapper,
+  StyledHeaderText,
+  StyledWelcomeText,
+} from 'Survey/Styles/Step0.style'
+
+export const Step1 = ({ gotoPrevStep, gotoNextStep }: StepProps) => {
+  return <div></div>
+}
