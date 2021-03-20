44d1e53775c50f1dab53f5ab6a39f39786fba183
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-03-20 00:51:55 -0400

    added logo, progress bar, title to step 1 page

--- frontend/src/modules/Survey/Components/Step1.tsx
+++ frontend/src/modules/Survey/Components/Step1.tsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 import { StepProps } from 'Survey/Types'
 import {
   StyledContainer,
+  StyledProgressBar,
   StyledLogo,
-  StyledWhiteActionText,
-  StyledTeamPic,
   StyledTitleWrapper,
-  StyledHeaderText,
   StyledWelcomeText,
+  StyledFullPanel,
 } from 'Survey/Styles/Step1.style'
 
 export const Step1 = ({ gotoPrevStep, gotoNextStep }: StepProps) => {
-  return <StyledContainer></StyledContainer>
+  return (
+    <StyledContainer>
+      <StyledFullPanel>
+        <StyledProgressBar />
+        <StyledLogo />
+        <StyledTitleWrapper>
+          <StyledWelcomeText>What do you identify as?</StyledWelcomeText>
+        </StyledTitleWrapper>
+      </StyledFullPanel>
+    </StyledContainer>
+  )
 }
--- frontend/src/modules/Survey/Styles/Step1.style.tsx
+++ frontend/src/modules/Survey/Styles/Step1.style.tsx
@@ -2,74 +2,68 @@ import styled, { css } from 'styled-components'
 
 import { colors, h1, h2, h3, StyledComponent } from '@core'
 
-import logo from '@assets/img/whitelogo.svg'
-import teamPic from '@assets/img/teamwork.svg'
-import whiteBackground from '@assets/img/whiterectangle.png'
+import logo from '@assets/img/purplelogo.svg'
+import prev from '@assets/img/prev.svg'
+import next from '@assets/img/next.svg'
+import progress from '@assets/img/progressbarstep1.svg'
 
 const Logo = ({ className }: StyledComponent) => (
   <div className={className}>
     <img src={logo} alt="logo" />
   </div>
 )
-export const StyledLogo = styled(Logo)`
-  margin: 40px;
-`
 
-const TeamPic = ({ className }: StyledComponent) => (
+const ProgressBar = ({ className }: StyledComponent) => (
   <div className={className}>
-    <img src={teamPic} alt="teamPic" />
+    <img src={progress} alt="progress" />
   </div>
 )
 
-// const WhiteBackground = ({ className }: StyledComponent) => (
-//   <div className={className}>
-//     <img src={whiteBackground} alt="whiteBackground" />
-//   </div>
-// )
+export const StyledProgressBar = styled(ProgressBar)`
+  justify-content: left;
+  align-content: left;
+`
 
-export const StyledTeamPic = styled(TeamPic)`
-  max-width: 100%;
-  margin-top: 108px;
+export const StyledLogo = styled(Logo)`
+  margin-top: 50px;
+  margin-left: 50px;
+  margin-bottom: 18px;
+  justify-content: left;
+  align-content: left;
 `
 
 export const StyledContainer = styled.div`
   height: 80%;
   width: 80%;
+  margin: 0;
   background-color: ${colors.white};
   box-shadow: -10px -10px 150px rgba(0, 0, 0, 0.1),
     10px 10px 150px rgba(0, 0, 0, 0.1);
-
   display: flex;
 `
 
 const panel = css`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `
 
-export const StyledWhiteActionText = styled.text`
-  ${h3};
-  color: ${colors.white};
+const fullPanel = css`
+  ${panel};
+  height: 100%;
+  width: 100%;
 `
 
 export const StyledTitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
-
-export const StyledHeaderText = styled.text`
-  ${h1};
-  color: ${colors.darkpurple};
-`
-
 export const StyledWelcomeText = styled.text`
   ${h2};
-  font-weight: 300;
-  color: ${colors.darkpurple};
+  font-weight: 500;
 `
-// export const StyledWhiteBackground = styled(WhiteBackground)`
-//   max-width: 100%;
-//   margin-top: 108px;
-// `
+
+export const StyledFullPanel = styled.div`
+  ${fullPanel}
+`
