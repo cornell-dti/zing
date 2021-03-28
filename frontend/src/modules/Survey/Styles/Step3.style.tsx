ef721d5c4e31c73e1b65a4bdfcc46c74a8504cfc
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-03-28 18:53:05 -0400

    got a working date picker with month and year defaulted to May 2024; gotta consult with design on whether this is fine

--- frontend/src/modules/Survey/Styles/Step3.style.tsx
+++ frontend/src/modules/Survey/Styles/Step3.style.tsx
@@ -1,92 +1,47 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import { colors, h2, h3 } from '@core'
 
-import { h2, h3, StyledComponent } from '@core'
-import { StyledContainer as Container } from 'Survey/Styles/StepTemplate.style'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
 
-import logo from '@assets/img/smallwhitelogo.svg'
-import check from '@assets/img/whitecheckmark.svg'
-
-const Logo = ({ className }: StyledComponent) => (
-  <div className={className}>
-    <img src={logo} alt="logo" />
-  </div>
-)
-
-const Check = ({ className }: StyledComponent) => (
-  <div className={className}>
-    <img src={check} alt="check" />
-  </div>
-)
-
-export const StyledLogoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+export const StyledContainer = styled.div`
+  margin: auto;
 `
 
-export const StyledLogo = styled(Logo)`
-  margin-top: 8.4%;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-
-export const StyledCheck = styled(Check)`
-  margin-right: 6%;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-
-export const StyledContainer = styled(Container)`
-  background: linear-gradient(
-    162.9deg,
-    #c794ee 0.78%,
-    #d9b6f6 27.99%,
-    #e8d6fb 55.75%,
-    #f6f3ff 101.78%
-  );
-`
-
-const panel = css`
-  display: flex;
-  flex-direction: column;
-`
-
-const fullPanel = css`
-  ${panel};
-  height: 100%;
-  width: 100%;
-`
-
-export const StyledFullPanel = styled.div`
-  ${fullPanel}
-`
-
-export const StyledCongratulationsWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 75%;
-  margin-top: 10%;
-`
-export const StyledCongratulationsText = styled.text`
+export const StyledText = styled.text`
   ${h2};
-  font-weight: 500;
-  color: white;
-`
-export const StyledContactWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10%;
+  color: ${colors.black};
 `
 
-export const StyledContactText = styled.text`
-  ${h3};
-  font-weight: 400;
-  color: rgba(172, 129, 206, 1);
+export const StyledCalendarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
+  justify-content: center;
+  align-items: center;
 `
+
+export const StyledTextField = withStyles({
+  root: {
+    background: 'white',
+    borderRadius: 3,
+    border: 0,
+    height: 48,
+    padding: '0 30px',
+    '& .MuiInput-underline:before': {
+      borderBottomColor: colors.darkpurple, // Semi-transparent underline
+    },
+    '& .MuiInput-underline:hover:before': {
+      borderBottomColor: colors.lightviolet, // Solid underline on hover
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: colors.darkpurple, // Solid underline on focus
+    },
+    // '& .MuiInputBase-root': {
+    //   color: colors.lightviolet,
+    // },
+    // '& .MuiInputBase-root:focus': {
+    //   color: colors.lightviolet,
+    // },
+  },
+})(TextField)
