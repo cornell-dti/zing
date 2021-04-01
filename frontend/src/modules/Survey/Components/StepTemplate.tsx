58b27155f119a179b0e37cc96e3ef04c01d4b001
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-04-01 19:13:03 -0400

    finished progress bar

--- frontend/src/modules/Survey/Components/StepTemplate.tsx
+++ frontend/src/modules/Survey/Components/StepTemplate.tsx
@@ -7,36 +7,42 @@ import {
   StyledFullPanel,
   StyledPrevButton,
   StyledNextButton,
+  StyledFullPanelNoPadding,
 } from 'Survey/Styles/StepTemplate.style'
 import prev from '@assets/img/prev.svg'
 import next from '@assets/img/next.svg'
+import ProgressBar from './UIElements/ProgressBar'
 
 export const StepTemplate: FunctionComponent<StepTemplateProps> = ({
+  stepNumber,
+  totalSteps,
   gotoPrevStep,
   gotoNextStep,
   children,
 }) => {
   return (
     <StyledContainer>
-      <StyledFullPanel>
-        {/* <StyledProgressBar /> */}
-        <StyledWrapper style={{ height: '10%' }}>
-          <StyledLogo />
-        </StyledWrapper>
-        <StyledWrapper style={{ height: '80%' }}>{children}</StyledWrapper>
-        <StyledWrapper style={{ height: '10%' }}>
-          <StyledPrevButton
-            className="prev"
-            src={prev}
-            onClick={gotoPrevStep}
-          />
-          <StyledNextButton
-            className="next"
-            src={next}
-            onClick={gotoNextStep}
-          />
-        </StyledWrapper>
-      </StyledFullPanel>
+      <StyledFullPanelNoPadding>
+        <ProgressBar stepNumber={stepNumber} totalSteps={totalSteps} />
+        <StyledFullPanel>
+          <StyledWrapper style={{ height: '10%' }}>
+            <StyledLogo />
+          </StyledWrapper>
+          <StyledWrapper style={{ height: '80%' }}>{children}</StyledWrapper>
+          <StyledWrapper style={{ height: '10%' }}>
+            <StyledPrevButton
+              className="prev"
+              src={prev}
+              onClick={gotoPrevStep}
+            />
+            <StyledNextButton
+              className="next"
+              src={next}
+              onClick={gotoNextStep}
+            />
+          </StyledWrapper>
+        </StyledFullPanel>
+      </StyledFullPanelNoPadding>
     </StyledContainer>
   )
 }
