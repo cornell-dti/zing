87f0e4c87c2b4a7860cb89f3636ebd25ed41c84f
Woosang Kang <wk228@cornell.edu>
2021-04-18 01:35:53 +0900

    added minimal user initiation

--- backend/functions/src/course/create-course.ts
+++ backend/functions/src/course/create-course.ts
@@ -12,20 +12,28 @@ const createCourse = functions.https.onRequest(
 			response.status(204).send("");
 		} else {
 			try {
-				const { courseId, courseName, studentList } = request.body;
+				const { courseId, studentList } = request.body;
 				const course: FirestoreCourseDoc = {
-					courseName,
 					studentList,
 					completed: [],
 				};
+				const groupDocRef = db.collection("course").doc(courseId);
+				let docExists = false;
 				// prettier-ignore
-				await db
-					.collection("course")
-					.doc(courseId)
-					.set(course, { merge: true });
+				await groupDocRef
+					.get()
+					.then((snapshot) => {
+						if (snapshot.exists) {
+							response.status(409).send("Specified groupId already exists!");
+							docExists = true;
+							return;
+						}
+						snapshot.ref.set(course, { merge: true });
+					});
+				if (docExists) return;
 				response
 					.status(200)
-					.send(`Successfully created a document for ${courseName}.`);
+					.send(`Successfully created a document for ${courseId}.`);
 			} catch (error) {
 				console.log(error);
 				response.status(500).send(error);
--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,9 +2,11 @@ import addResponse from "./user/add-response";
 import createCourse from "./course/create-course";
 import createSurveyCsv from "./course/data-to-csv";
 import seedSurveyData from "./user/add-response-batch";
+import addUser from "./user/add-user";
 
 export const newCourse = createCourse;
 export const newSurvey = addResponse;
 export const newCsv = createSurveyCsv;
+export const newUser = addUser;
 
 export const seedData = seedSurveyData;
