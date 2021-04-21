4e2be26dd698fb25f58aec7e9720a5781b61137e
Woosang Kang <wk228@cornell.edu>
2021-04-21 11:38:44 +0900

    added endpoint to link user's group config with actual groups

--- backend/functions/src/course/data-to-csv.ts
+++ backend/functions/src/course/data-to-csv.ts
@@ -20,7 +20,7 @@ const fields = [
 	"start",
 ];
 
-const createSurveyCsv = functions.https.onRequest(
+const addSurveyCsv = functions.https.onRequest(
 	async (request: functions.https.Request, response: functions.Response) => {
 		response.set("Access-Control-Allow-Origin", "*");
 		if (request.method === "OPTIONS") {
@@ -28,59 +28,66 @@ const createSurveyCsv = functions.https.onRequest(
 			response.set("Access-Control-Allow-Headers", "Content-Type");
 			response.status(204).send("");
 		} else {
-			try {
-				const { courseId } = request.body;
-				const surveySnapshot = await db
-					.collection("course")
-					.doc(courseId)
-					.collection("survey")
-					.get();
-
-				// prettier-ignore
-				const surveyDocs = surveySnapshot.docs.map(
-					(doc: DocumentSnapshot) => doc.data()
-				);
-
-				const outputCsv = await parseAsync(surveyDocs, { fields });
-				const filename = `${courseId}.csv`;
-				const tempFile = path.join(os.tmpdir(), filename);
-
-				return new Promise<void>((resolve, reject) => {
-					fs.writeFile(tempFile, outputCsv, (error) => {
-						if (error) {
-							reject(error);
-							return;
-						}
-						const bucket = storage.bucket();
-
-						bucket
-							.upload(tempFile, {
-								metadata: {
-									metadata: {
-										firebaseStorageDownloadTokens: uuidv4(),
-									},
-								},
-							})
-							.then(() => {
-								response.status(200).send(
-									`Successfully converted responses\
-										for ${courseId} to a CSV file.`
-								);
-								resolve();
-							})
-							.catch((error) => {
-								console.log(error);
-								response.status(500).send(error);
-								reject(error);
-							});
-					});
-				});
-			} catch (error) {
-				console.log(error);
-				response.status(500).send(error);
-			}
+			createSurveyCsv(request, response);
 		}
 	}
 );
 
-export default createSurveyCsv;
+const createSurveyCsv = async (
+	request: functions.https.Request,
+	response: functions.Response<any>
+) => {
+	try {
+		const { courseId } = request.body;
+		const surveySnapshot = await db
+			.collection("course")
+			.doc(courseId)
+			.collection("survey")
+			.get();
+
+		// prettier-ignore
+		const surveyDocs = surveySnapshot.docs.map(
+			(doc: DocumentSnapshot) => doc.data()
+		);
+
+		const outputCsv = await parseAsync(surveyDocs, { fields });
+		const filename = `${courseId}.csv`;
+		const tempFile = path.join(os.tmpdir(), filename);
+
+		return new Promise<void>((resolve, reject) => {
+			fs.writeFile(tempFile, outputCsv, (error) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				const bucket = storage.bucket();
+
+				bucket
+					.upload(tempFile, {
+						metadata: {
+							metadata: {
+								firebaseStorageDownloadTokens: uuidv4(),
+							},
+						},
+					})
+					.then(() => {
+						response.status(200).send(
+							`Successfully converted responses\
+								for ${courseId} to a CSV file.`
+						);
+						resolve();
+					})
+					.catch((error) => {
+						console.log(error);
+						response.status(500).send(error);
+						reject(error);
+					});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+		response.status(500).send(error);
+	}
+};
+
+export default addSurveyCsv;
