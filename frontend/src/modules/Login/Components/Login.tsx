1e5577a8b447b275a13aa0681745ebe96033ced5
Changyuan Lin <coreballium@outlook.com>
2021-04-21 18:47:49 -0400

    Somewhat styled login header, fields

--- frontend/src/modules/Login/Components/Login.tsx
+++ frontend/src/modules/Login/Components/Login.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { StyledContainer } from 'Login/Styles/Login.style'
+import {
+  StyledBackground,
+  StyledContainer,
+  StyledHeader,
+  StyledHeaderText,
+  StyledWelcomeText,
+  StyledFields,
+} from 'Login/Styles/Login.style'
+import { EmailField, PasswordField } from '@core/Components'
 
 export const Login = () => {
-  return <StyledContainer />
+  // Email and password props
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <StyledBackground>
+      <StyledContainer>
+        <StyledHeader>
+          <StyledHeaderText>Log in</StyledHeaderText>
+          <StyledWelcomeText>Welcome back!</StyledWelcomeText>
+        </StyledHeader>
+        <StyledFields>
+          <EmailField
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <PasswordField
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </StyledFields>
+      </StyledContainer>
+    </StyledBackground>
+  )
 }
