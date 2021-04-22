302470bc532bc3e1b6234f549665498b1e2e2125
Changyuan Lin <coreballium@outlook.com>
2021-04-21 21:52:26 -0400

    Create login button

--- frontend/src/modules/Login/Components/Login.tsx
+++ frontend/src/modules/Login/Components/Login.tsx
@@ -3,35 +3,53 @@ import React, { useState } from 'react'
 import {
   StyledBackground,
   StyledContainer,
+  StyledCenter,
   StyledHeader,
   StyledHeaderText,
   StyledWelcomeText,
   StyledFields,
 } from 'Login/Styles/Login.style'
 import { EmailField, PasswordField } from '@core/Components'
+import { colors } from '@core/Constants'
+import { LoginButton } from 'Login/Components/UIElements/LoginButton'
 
 export const Login = () => {
   // Email and password props
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const textContainerStyle = {
+    width: '388px',
+  }
+
+  const textInputStyle = {
+    color: colors.darkpurple,
+  }
+
   return (
     <StyledBackground>
       <StyledContainer>
-        <StyledHeader>
-          <StyledHeaderText>Log in</StyledHeaderText>
-          <StyledWelcomeText>Welcome back!</StyledWelcomeText>
-        </StyledHeader>
-        <StyledFields>
-          <EmailField
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <PasswordField
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </StyledFields>
+        <StyledCenter>
+          <StyledHeader>
+            <StyledHeaderText>Log in</StyledHeaderText>
+            <StyledWelcomeText>Welcome back!</StyledWelcomeText>
+          </StyledHeader>
+          <StyledFields>
+            <EmailField
+              containerStyle={textContainerStyle}
+              inputStyle={textInputStyle}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordField
+              containerStyle={textContainerStyle}
+              inputStyle={textInputStyle}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </StyledFields>
+          <LoginButton onClick={() => {}} />
+        </StyledCenter>
       </StyledContainer>
     </StyledBackground>
   )
