569dacf72d9808c018189bdcf15d76c14dc25c19
Changyuan Lin <coreballium@outlook.com>
2021-04-27 22:31:59 -0400

    Update login fields to use new fields

--- frontend/src/modules/Core/Components/InputFields/PasswordField.tsx
+++ frontend/src/modules/Core/Components/InputFields/PasswordField.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 
+import { colors } from '@core'
 import { InputField } from '@core/Components/InputField'
 import { InputProps } from '@core/Types/FormFieldProps'
 
 export const PasswordField = ({
+  fullWidth = true,
+  endAdornment,
+  key = 'default',
+  MuiColor = colors.darkpurple,
   containerStyle,
   inputStyle,
   placeholder = 'Password',
   value,
   onChange,
   disabled,
+  error,
 }: InputProps) => {
   return (
     <InputField
+      fullWidth={fullWidth}
+      endAdornment={endAdornment}
+      key={key}
+      MuiColor={MuiColor}
       containerStyle={containerStyle}
       inputStyle={inputStyle}
       value={value}
@@ -20,6 +30,7 @@ export const PasswordField = ({
       type="password"
       placeholder={placeholder}
       disabled={disabled}
+      error={error}
     />
   )
 }
