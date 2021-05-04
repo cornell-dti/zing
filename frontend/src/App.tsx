fa0a5073c9f54e729454d8a95be8e437fe8766a5
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-05-03 21:55:19 -0400

    set up routing and main component for dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import { HOME_PATH, SURVEY_PATH, CREATE_GROUP_PATH } from '@core'
+import {
+  HOME_PATH,
+  SURVEY_PATH,
+  CREATE_GROUP_PATH,
+  EDIT_ZING_PATH,
+} from '@core'
 
 import { Home } from 'Home'
 import { Survey } from 'Survey'
-import { CreateGroupForm } from '../src/modules/CreateGroup/Components/Form'
+import { CreateGroupForm } from 'CreateGroup'
+import { Dashboard } from 'EditZing'
+
 import './App.css'
 
 const App = () => {
@@ -15,6 +22,7 @@ const App = () => {
         <Route exact path={HOME_PATH} component={Home} />
         <Route exact path={SURVEY_PATH} component={Survey} />
         <Route exact path={CREATE_GROUP_PATH} component={CreateGroupForm} />
+        <Route exact path={EDIT_ZING_PATH} component={Dashboard} />
       </Switch>
     </Router>
   )
---
+++ frontend/src/modules/EditZing/Components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledContainer } from '../Styles/DashboardStyle.style'
+
+export const Dashboard = () => {
+  return (
+    <StyledContainer>
+      <p>hello</p>
+    </StyledContainer>
+  )
+}
