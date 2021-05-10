240bebe2d1cebed4293a2dfe2527a2b948022686
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-05-10 15:41:50 -0400

    fixed Zing vs. group nomenclature on routes, global var names, and createZing form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import {
-  HOME_PATH,
-  SURVEY_PATH,
-  CREATE_GROUP_PATH,
-  EDIT_ZING_PATH,
-} from '@core'
+import { HOME_PATH, SURVEY_PATH, CREATE_ZING_PATH, EDIT_ZING_PATH } from '@core'
 
 import { Home } from 'Home'
 import { Survey } from 'Survey'
-import { CreateGroupForm } from 'CreateGroup'
+import { CreateZingForm } from 'CreateZing'
 import { Dashboard } from 'EditZing'
 
 import './App.css'
@@ -21,7 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path={HOME_PATH} component={Home} />
         <Route exact path={SURVEY_PATH} component={Survey} />
-        <Route exact path={CREATE_GROUP_PATH} component={CreateGroupForm} />
+        <Route exact path={CREATE_ZING_PATH} component={CreateZingForm} />
         <Route exact path={EDIT_ZING_PATH} component={Dashboard} />
       </Switch>
     </Router>
--- frontend/src/modules/Core/Constants/Routes.ts
+++ frontend/src/modules/Core/Constants/Routes.ts
@@ -1,4 +1,4 @@
 export const HOME_PATH = '/'
 export const SURVEY_PATH = '/survey'
-export const CREATE_GROUP_PATH = '/creategroup'
+export const CREATE_ZING_PATH = '/createzing'
 export const EDIT_ZING_PATH = '/editzing'
