9b2c72d973ba40e3c7e59360d4368765bd9583be
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-06-02 12:33:54 -0400

    Changed name from fakestudentgroups to just studentgroups

--- frontend/src/modules/EditZing/Components/Dashboard.tsx
+++ frontend/src/modules/EditZing/Components/Dashboard.tsx
@@ -8,9 +8,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 
 export const Dashboard = () => {
   const fakeStudentGroupsFromJson: Student[][] = require('EditZing/fakeData.json')
-  const [fakeStudentGroups, setFakeStudentGroups] = useState(
-    fakeStudentGroupsFromJson
-  )
+  const [studentGroups, setStudentGroups] = useState(fakeStudentGroupsFromJson)
 
   /** This function moves a student from one grid to a destination grid based
    * on a starting and destination grid index */
@@ -23,20 +21,20 @@ export const Dashboard = () => {
     // console.log('before processing:')
     // console.log(fakeStudentGroups)
     var newData: Student[][] = []
-    fakeStudentGroups.map((studentList, index) => {
+    studentGroups.map((studentList, index) => {
       var groupToPush: Student[] = []
       if (index == startingIndex) {
-        groupToPush = fakeStudentGroups[startingIndex].filter(
+        groupToPush = studentGroups[startingIndex].filter(
           (student) => student.studentId != studentToMove.studentId
         )
       } else if (index == destinationIndex) {
-        groupToPush = fakeStudentGroups[destinationIndex].concat(studentToMove)
+        groupToPush = studentGroups[destinationIndex].concat(studentToMove)
       } else {
         groupToPush = studentList
       }
       newData.push(groupToPush)
     })
-    setFakeStudentGroups(newData)
+    setStudentGroups(newData)
     // console.log('post-processing:')
     // console.log(newData)
   }
@@ -45,10 +43,10 @@ export const Dashboard = () => {
     <StyledContainer>
       <DndProvider backend={HTML5Backend}>
         <Grid container spacing={1}>
-          {fakeStudentGroups.map((fakeStudentGroup, index) => (
+          {studentGroups.map((studentGroup, index) => (
             <GroupGrid
               key={index}
-              studentList={fakeStudentGroup}
+              studentList={studentGroup}
               groupIndex={index}
               moveStudent={moveStudent}
             />
