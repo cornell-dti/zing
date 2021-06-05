51a275c05c4a343b20fbf0968b61915b001c01f3
Shi Chong <sz329@cornell.edu>
2021-06-05 06:52:34 -0400

    removing a bunch of extraneous imports

---
+++ frontend/src/modules/Dashboard/Components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  StyledOuterContainer,
+  StyledContainer,
+} from '../Styles/Dashboard.style'
+
+export const Dashboard = () => {
+  return (
+    <StyledOuterContainer>
+      <StyledContainer></StyledContainer>
+    </StyledOuterContainer>
+  )
+}
