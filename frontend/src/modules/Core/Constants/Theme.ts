b922d31b168c4eb0f80295efe35024c775790eab
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-06-29 16:59:11 -0700

    added first filter (metric)

--- frontend/src/modules/Core/Constants/Theme.ts
+++ frontend/src/modules/Core/Constants/Theme.ts
@@ -1,4 +1,5 @@
 export const colors = {
+  mediumviolet: '#C794EE',
   verylightviolet: '#FCFAFF',
   black: '#222222',
   white: '#FFFFFF',
@@ -6,4 +7,6 @@ export const colors = {
   lightviolet: '#E5CEFA',
   red: '#FF6584',
   yellow: '#FFFFB1',
+  lightgreen: '#CEF5D6',
+  darkgreen: '#157E2C',
 }
