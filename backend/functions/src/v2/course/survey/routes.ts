fd16a6744de6c368105829a2d61490e8e9f2e834
Woosang Kang <woosang@bearrobotics.ai>
2021-07-18 20:59:48 +0900

    added survey-related endpoints

---
+++ backend/functions/src/v2/course/survey/routes.ts
@@ -0,0 +1,17 @@
+import * as express from "express";
+import { Request, Response } from "express";
+import { postSurvey, getSurvey } from "./functions";
+import { internalError } from "../../../common/utils";
+
+// eslint-disable-next-line new-cap
+const router: express.Router = express.Router({ mergeParams: true });
+
+router.post("/", (req: Request, res: Response) => {
+	postSurvey(req, res).catch((err) => internalError(res, err));
+});
+
+router.get("/:studentId", (req: Request, res: Response) => {
+	getSurvey(req, res).catch((err) => internalError(res, err));
+});
+
+export default router;
