e71f49e6312871e919776cc366ddb45466ab4013
Woosang Kang <woosang@bearrobotics.ai>
2021-07-18 21:02:03 +0900

    updated index.ts to include v2 endpoints

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,7 +2,7 @@
  * ==================== V1 Code =======================
  */
 
-import addResponse from "./v1/user/add-response";
+/* import addResponse from "./v1/user/add-response";
 import addCourse from "./v1/course/add-course";
 import addSurveyCsv from "./v1/course/data-to-csv";
 import seedSurveyData from "./v1/user/add-response-batch";
@@ -19,7 +19,7 @@ export const newConfig = addGroupConfig;
 export const linkConfig = connectGroupConfig;
 export const signUpUser = signUp;
 export const signInUser = signIn;
-export const seedData = seedSurveyData;
+export const seedData = seedSurveyData; */
 
 /**
  * ==================== V2 Code =======================
@@ -33,6 +33,8 @@ const app = express();
 
 // import all routers
 import courseRoutes from "./v2/course/routes";
+import instructorRoutes from "./v2/instructor/routes";
+import devRoutes from "./dev/routes";
 
 // Automatically allow cross-origin requests. NEED TO CHANGE.
 app.use(cors({ origin: true }));
@@ -44,5 +46,7 @@ app.get("/test", (req, res) => res.send("yo"));
 
 // set up routes
 app.use("/course", courseRoutes);
+app.use("/instructor", instructorRoutes);
+app.use("/dev", devRoutes);
 
 export const api = functions.https.onRequest(app);
