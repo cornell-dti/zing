d55c13dd6463b989b9837735ed7d0a6e2062260a
Woosang Kang <woosang@bearrobotics.ai>
2021-07-18 21:47:02 +0900

    removed commented-out v1 legacy code from index.ts

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,30 +1,3 @@
-/**
- * ==================== V1 Code =======================
- */
-
-/* import addResponse from "./v1/user/add-response";
-import addCourse from "./v1/course/add-course";
-import addSurveyCsv from "./v1/course/data-to-csv";
-import seedSurveyData from "./v1/user/add-response-batch";
-import addUser from "./v1/user/add-user";
-import addGroupConfig from "./v1/user/add-group-config";
-import connectGroupConfig from "./v1/course/connect-group-config";
-import { signIn, signUp } from "./v1/auth";
-
-export const newCourse = addCourse;
-export const newSurvey = addResponse;
-export const newCsv = addSurveyCsv;
-export const newUser = addUser;
-export const newConfig = addGroupConfig;
-export const linkConfig = connectGroupConfig;
-export const signUpUser = signUp;
-export const signInUser = signIn;
-export const seedData = seedSurveyData; */
-
-/**
- * ==================== V2 Code =======================
- */
-
 import * as express from "express";
 import * as cors from "cors";
 import * as functions from "firebase-functions";
