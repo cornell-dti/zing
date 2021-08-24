860afb950e5f0c50d791e90c183d9d36ca618b77
Woosang Kang <wk228@cornell.edu>
2021-08-24 21:50:35 +0900

    added cors allowance

--- backend/groupeng/main.py
+++ backend/groupeng/main.py
@@ -6,6 +6,24 @@ from operator import itemgetter
 
 
 def create_groups(request):
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     # Setup logging
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logger = logging.getLogger(__name__)
