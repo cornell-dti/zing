e4a6d6442b3464159b2447e629fe377b39649470
Will Zhang <wz282@cornell.edu>
2023-03-17 15:19:24 -0400

    try to fix js build errs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
 
-export default defineConfig({
+export default defineConfig(() => ({
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.ts': 'tsx',
+      },
+    },
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-})
+}))
