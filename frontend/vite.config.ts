72dd19cd873cb2aa60d022ad3f7d43ac3e059797
Will Zhang <wz282@cornell.edu>
2023-04-10 17:26:25 -0400

    dns uncommented in config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
 
-// ! Miranda: why did you add this dns?
 // @ts-ignore
-// import dns from 'dns'
-// dns.setDefaultResultOrder('verbatim')
+import dns from 'dns'
+// @ts-ignore
+dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig(() => ({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
